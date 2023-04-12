3b1f32de95eb23d35cffc724b8ced742aa834aa7
Ashith Shetty <ashitdshetty1101@gmail.com>
2023-04-12 16:09:26 +0530

    added landing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Registration from "./pages/Registration";
+import Landing from "./pages/Landing";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Landing />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/registration" element={<Registration />} />
       </Routes>
     </Router>
