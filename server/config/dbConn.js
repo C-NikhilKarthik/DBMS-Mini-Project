5704615988a25d386179fc448e6e791f67568dcd
Avaneesh Sundararajan <avaneeshsundar@gmail.com>
2023-04-13 18:56:11 +0530

    fix:removed unneccessary files

--- server/config/dbConn.js
+++ server/config/dbConn.js
@@ -1,38 +1,19 @@
-// const {MongoClient}=require('mongodb')
+const mysql=require('mysql')
+const con=mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"avisql@123",
+    database:"dreamhome",
+    multipleStatements:"true"
+})
 
-// let dbConnection
-
-// /*connectToDb is for connecting to database and getDb will return the database connection*/
-// module.exports={
-//     connectToDb:(call_back)=>{
-
-//         let url2='mongodb://localhost:27017/Project'
-//         let url='mongodb+srv://AvaneeshS:aviproject%40123@cluster0.rpkvd1e.mongodb.net/project'
-//         MongoClient.connect(url)
-//         .then((client)=>{
-//             dbConnection=client.db()
-//             return call_back()
-//         })
-//         .catch(err=>{
-//             console.log(err)
-//             return call_back()
-//         })
-//     },
-//     getDb:()=>dbConnection
-// }
-
-require('dotenv').config()
-
-const mongoose=require('mongoose')
-const connectDB=async()=>{
-    try{
-        
-        await mongoose.connect(process.env.DATABASE_URI)
-        console.log("connected to database")
+// Check if the connection is established successfully
+con.connect((err) => {
+    if (err) {
+      console.error('Error connecting to MySQL database:', err);
+      return;
     }
-    catch(err){
-        console.log(err)
-    }
-}
-
-module.exports=connectDB
+    console.log('Connected to MySQL database!');
+  });
+  
+module.exports=con
