6f113cbbc7e8605cf6c18ad5151e52a4b8f6db7a
Ashith Shetty <ashitdshetty1101@gmail.com>
2023-04-15 13:01:28 +0530

    fix:routing of dropdown

--- client/src/components/Dropdown.jsx
+++ client/src/components/Dropdown.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Dropdown = () => {
   const [showMenu, setShowMenu] = useState(false);
@@ -17,30 +18,30 @@ const Dropdown = () => {
       </button>
       {showMenu && (
         <div className="absolute z-50 mt-2 py-2 w-48 bg-white  rounded-md shadow-lg">
-          <a
-            href="/registration/client"
+          <Link
+            to="/registration/client"
             className="block px-4 py-2 text-gray-800 hover:bg-gray-100"
           >
             Client Registration
-          </a>
-          <a
-            href="/registration/owner"
+          </Link>
+          <Link
+            to="/registration/owner"
             className="block px-4 py-2 text-gray-800 hover:bg-gray-100"
           >
             Owner Registration
-          </a>
-          <a
-            href="/registration/property"
+          </Link>
+          <Link
+            to="/registration/property"
             className="block px-4 py-2 text-gray-800 hover:bg-gray-100"
           >
             Property Registration
-          </a>
-          <a
-            href="/registration/staff"
+          </Link>
+          <Link
+            to="/registration/staff"
             className="block px-4 py-2 text-gray-800 hover:bg-gray-100"
           >
             Staff Registration
-          </a>
+          </Link>
         </div>
       )}
     </div>
